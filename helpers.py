3a006dad0318b4b7469c5da879f23d80be62f5b4
zjovicic <91837425+zjovicic@users.noreply.github.com>
2023-11-13 11:03:56 +0100

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,31 @@
+from flask import redirect, render_template, session
+from functools import wraps
+
+# This function handles displaying of apology messages in case that user has made some errors when entering the data in some of the forms used on the website.
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+# This function handles displaying of apology messages in case that user has made some errors when entering the data in some of the forms used on the website.
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
